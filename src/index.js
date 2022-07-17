d93c4d5d47d08f30e0f03074431f26cfb7b52fb1
AegisVP <vlad@pysarenko.com>
2022-07-17 04:51:41 +0200

    created own modal handle class

--- src/index.js
+++ src/index.js
@@ -1,152 +1,180 @@
-import fetchFilmGenres from './js/fetchFilmGenres';
-import renderGallery from './js/renderGallery';
-import { saveDataToStorage } from './js/dataStorage';
-import fetchFilmData from './js/fetchFilmData';
+// import fetchFilmGenres from './js/fetchFilmGenres';
+// import renderGallery from './js/renderGallery';
+// import { saveDataToStorage } from './js/dataStorage';
+// import fetchFilmData from './js/fetchFilmData';
 import showLoader from './js/loader';
-import micromodal from 'micromodal';
-import { auth, signInWithEmailAndPassword, signOut } from './js/firebase__init';
+// import micromodal from 'micromodal';
+// import { auth, signInWithEmailAndPassword, signOut } from './js/firebase__init';
 
-micromodal.init({
-  onShow: modal => {
-    if (modal.id !== 'modal-login') return;
+// micromodal.init({
+//   onShow: modal => {
+//     if (modal.id !== 'modal-login') return;
 
-    const user = auth.currentUser;
-    // console.log('user', user);
-    const formEl = document.querySelector('form.modal-login-form');
+//     const user = auth.currentUser;
+//     // console.log('user', user);
+//     const formEl = document.querySelector('form.modal-login-form');
 
-    if (user !== null) {
-      formEl.innerHTML = `
-          <p> You are already signed in as ${user.email}</p>
-          <button class="button button--accent" type="submit">Log-out</button>`;
+//     if (user !== null) {
+//       formEl.innerHTML = `
+//           <p> You are already signed in as ${user.email}</p>
+//           <button class="button button--accent" type="submit">Log-out</button>`;
 
-      formEl.addEventListener('submit', doLogout);
-    } else {
-      formEl.innerHTML = `
-          <label>Username: <input type="email" name="email" autocomplete="username" class="login-form__input"></label>
-          <label>Password: <input type="password" name="password" autocomplete="current-password" class="login-form__input"></label>
-          <button class="button button--accent" type="submit">Log-in</button>`;
+//       formEl.addEventListener('submit', doLogout);
+//     } else {
+//       formEl.innerHTML = `
+//           <label>Username: <input type="email" name="email" autocomplete="username" class="login-form__input"></label>
+//           <label>Password: <input type="password" name="password" autocomplete="current-password" class="login-form__input"></label>
+//           <button class="button button--accent" type="submit">Log-in</button>`;
 
-      formEl.addEventListener('submit', doLogin);
-    }
-  },
-});
+//       formEl.addEventListener('submit', doLogin);
+//     }
+//   },
+// });
 
-function doLogout(e) {
-  e.preventDefault();
+// function doLogout(e) {
+//   e.preventDefault();
 
-  signOut(auth)
-    .then(() => {
-      e.preventDefault();
+//   signOut(auth)
+//     .then(() => {
+//       e.preventDefault();
 
-      document
-        .querySelector('form.modal-login-form')
-        .removeEventListener('submit', doLogin);
+//       document
+//         .querySelector('form.modal-login-form')
+//         .removeEventListener('submit', doLogin);
 
-      localStorage.removeItem('useruid');
+//       localStorage.removeItem('useruid');
 
-      micromodal.close('modal-login');
-    })
-    .catch(error => {
-      const errorCode = error.code;
-      // const errorMessage = error.message;
-      window.alert(`errorCode: ${errorCode}`);
-      document
-        .querySelector('form.modal-login-form')
-        .addEventListener('submit', doLogin);
-    });
-}
+//       micromodal.close('modal-login');
+//     })
+//     .catch(error => {
+//       const errorCode = error.code;
+//       // const errorMessage = error.message;
+//       window.alert(`errorCode: ${errorCode}`);
+//       document
+//         .querySelector('form.modal-login-form')
+//         .addEventListener('submit', doLogin);
+//     });
+// }
 
-function doLogin(e) {
-  e.preventDefault();
+// function doLogin(e) {
+//   e.preventDefault();
 
-  // console.log('doLogin started');
-  // console.dir(e.currentTarget.elements.email.value);
-  const email = e.currentTarget.elements.email.value;
-  const password = e.currentTarget.elements.password.value;
+//   // console.log('doLogin started');
+//   // console.dir(e.currentTarget.elements.email.value);
+//   const email = e.currentTarget.elements.email.value;
+//   const password = e.currentTarget.elements.password.value;
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
-      // Signed in
-      const user = userCredential.user;
+//   signInWithEmailAndPassword(auth, email, password)
+//     .then(userCredential => {
+//       // Signed in
+//       const user = userCredential.user;
 
-      // console.log(user.uid);
-      saveDataToStorage('useruid', user.uid);
+//       // console.log(user.uid);
+//       saveDataToStorage('useruid', user.uid);
 
-      micromodal.close('modal-login');
-      window.alert(`Loged in as ${user.email}`);
-      document
-        .querySelector('form.modal-login-form')
-        .removeEventListener('submit', doLogin);
-    })
-    .catch(error => {
-      const errorCode = error.code;
-      // const errorMessage = error.message;
-      window.alert(`errorCode: ${errorCode}`);
-    });
-}
+//       micromodal.close('modal-login');
+//       window.alert(`Loged in as ${user.email}`);
+//       document
+//         .querySelector('form.modal-login-form')
+//         .removeEventListener('submit', doLogin);
+//     })
+//     .catch(error => {
+//       const errorCode = error.code;
+//       // const errorMessage = error.message;
+//       window.alert(`errorCode: ${errorCode}`);
+//     });
+// }
 
-// Посилання на елементи сторінки
-const galleryEl = document.querySelector('.gallery');
-const searchForm = document.querySelector('.search-bar');
+// // Посилання на елементи сторінки
+// const galleryEl = document.querySelector('.gallery');
+// const searchForm = document.querySelector('.search-bar');
 
 // Вішаєм слухача на searchForm
-searchForm.addEventListener('submit', onSearch);
+// searchForm.addEventListener('submit', onSearch);
 
-// Отримання переліку усіх жанрів фільмів та запис їх до локального сховища
-fetchFilmGenres({}).then(({ genres }) => {
-  saveDataToStorage('genres', genres);
-});
+// // Отримання переліку усіх жанрів фільмів та запис їх до локального сховища
+// fetchFilmGenres({}).then(({ genres }) => {
+//   saveDataToStorage('genres', genres);
+// });
 
-// Отримання даних про популярні фільми (перша сторінка),
-// запис їх до локального сховища та розміщення на сторінці
-const currentPage = window.location;
-// console.log(currentPage);
+// // Отримання даних про популярні фільми (перша сторінка),
+// // запис їх до локального сховища та розміщення на сторінці
+// const currentPage = window.location;
+// // console.log(currentPage);
 
-switch (currentPage.pathname) {
-  case '/library.html':
-    populateLibraryHtml();
-    break;
-  default:
-    populateIndexHtml();
-    break;
-}
+// switch (currentPage.pathname) {
+//   case '/library.html':
+//     populateLibraryHtml();
+//     break;
+//   default:
+//     populateIndexHtml();
+//     break;
+// }
 
-function populateIndexHtml() {
-  fetchFilmData({}).then(({ results }) => {
-    saveDataToStorage('requestResults', results);
-    renderGallery({
-      data: results,
-      elementRef: galleryEl,
-      onClick: event => {
-        // Дана функція задана для прикладу, потребує написання логіки відкривання модального вікна
-        //Перевірка, що клікнули саме на картинку
-        if (event.target.nodeName === 'IMG') {
-          //Виведення id картки, що відповідає id фільму на сервері
-          console.log(event.target.parentNode.dataset.id);
-          alert('Функцію не задано!');
-        }
-      },
-    });
-    showLoader(false);
+// function populateIndexHtml() {
+//   fetchFilmData({}).then(({ results }) => {
+//     saveDataToStorage('requestResults', results);
+//     renderGallery({
+//       data: results,
+//       elementRef: galleryEl,
+//       onClick: event => {
+//         // Дана функція задана для прикладу, потребує написання логіки відкривання модального вікна
+//         //Перевірка, що клікнули саме на картинку
+//         if (event.target.nodeName === 'IMG') {
+//           //Виведення id картки, що відповідає id фільму на сервері
+//           console.log(event.target.parentNode.dataset.id);
+//           alert('Функцію не задано!');
+//         }
+//       },
+//     });
+//     showLoader(false);
+//   });
+// }
+
+// function populateLibraryHtml() {
+//   showLoader(false);
+// }
+
+// function onSearch(e) {
+//   e.preventDefault();
+//   const request = e.target.search.value.trim().toLowerCase();
+//   saveDataToStorage('searchQuery', request);
+//   fetchFilmData({ page: '1', query: `${request}`, isSearch: 'true' }).then(
+//     ({ results }) => {
+//       saveDataToStorage('requestResults', results);
+//       renderGallery({
+//         data: results,
+//         elementRef: galleryEl,
+//       });
+//       showLoader(false);
+//     }
+//   );
+// }
+
+showLoader(false);
+
+import MyModal from './js/mymodal';
+
+const modalDevRef = document.getElementById('modal-dev');
+const modalAuthRef = document.getElementById('modal-login');
+// console.log(modalAuthRef);
+
+if (modalDevRef) {
+  const modalDev = new MyModal({
+    modalRef: modalDevRef,
   });
-}
 
-function populateLibraryHtml() {
-  showLoader(false);
+  document
+    .querySelector('[data-open-modal-dev]')
+    .addEventListener('click', modalDev.openModal.bind(modalDev));
 }
+if (modalAuthRef) {
+  const modalAuth = new MyModal({
+    modalRef: modalAuthRef,
+  });
 
-function onSearch(e) {
-  e.preventDefault();
-  const request = e.target.search.value.trim().toLowerCase();
-  saveDataToStorage('searchQuery', request);
-  fetchFilmData({ page: '1', query: `${request}`, isSearch: 'true' }).then(
-    ({ results }) => {
-      saveDataToStorage('requestResults', results);
-      renderGallery({
-        data: results,
-        elementRef: galleryEl,
-      });
-      showLoader(false);
-    }
-  );
+  document
+    .querySelector('[data-open-modal-login]')
+    .addEventListener('click', modalAuth.openModal.bind(modalAuth));
 }
+// devModal.openModal();
---
+++ src/js/mymodal.js
@@ -0,0 +1,41 @@
+export default class MyModal {
+  constructor({ showClass = 'is-open', modalRef }) {
+    if (!modalRef) {
+      console.error(
+        'MyModal is initialized without modal refference:' + modalRef
+      );
+      return;
+    }
+
+    this.showClass = showClass;
+    this.modalRef = modalRef;
+
+    this.modalRef.addEventListener('click', this.closeModal.bind(this));
+    this.modalRef
+      .querySelectorAll('[data-mymodal-close]')
+      .forEach(r => r.addEventListener('click', this.closeModal.bind(this)));
+  }
+
+  openModal(e) {
+    e.preventDefault();
+    // console.log('opening modal');
+    this.modalRef.classList.add(this.showClass);
+    this.modalRef.removeAttribute('aria-hidden');
+
+    window.addEventListener('keydown', this.monitorEscKey.bind(this));
+  }
+
+  closeModal({ type, target, currentTarget }) {
+    if (type === 'click' && target !== currentTarget) return;
+
+    this.modalRef.classList.remove(this.showClass);
+    this.modalRef.setAttribute('aria-hidden', true);
+    window.removeEventListener('keydown', this.monitorEscKey.bind(this));
+  }
+
+  monitorEscKey(e) {
+    if (e.code !== 'Escape') return;
+
+    this.closeModal(e);
+  }
+}
