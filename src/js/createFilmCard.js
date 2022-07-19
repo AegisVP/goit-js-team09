4b994194cc38eca550a47b8d2b679f7ce96d5f8c
DaryaGrygorova <grygorova.darya@gmail.com>
2022-07-19 12:10:39 +0300

    add default string in card if data of film include empty fields

--- src/js/createFilmCard.js
+++ src/js/createFilmCard.js
@@ -7,12 +7,12 @@ import { fetchDataFromStorage } from './dataStorage';
 // Для цього при виклику необхідно вказати другим параметром 'true'
 
 const createFilmCard = (
-  { id, vote_average, release_date, poster_path, title, genre_ids },
+  { id, vote_average, release_date, poster_path, title, genre_ids, original_title },
   isLibrary = false
 ) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/';
   const rating = isLibrary
-    ? `<p class="filmCard-rating">${vote_average.toFixed(1)}</p>`
+    ? `<p class="filmCard-rating">${vote_average ? vote_average.toFixed(1) : '-'}</p>`
     : '';
 
   const imgSrc = width => {
@@ -30,12 +30,12 @@ const createFilmCard = (
             src="${imgSrc(400)}"
             width="280"
             height="398"
-            alt="${title}"
+            alt="${title ? title : 'Poster'}"
             data-action="open-modal"/>
-        <p class="filmCard-title">${title}</p>
+        <p class="filmCard-title">${title ? title : (original_title ? original_title : 'No name' )}</p>
         <div class="filmCard-description">
-            <p class="filmCard-genres">${getFilmGenres(genre_ids)}</p>
-            <p class="filmCard-release">${release_date.slice(0, 4)}</p>
+            <p class="filmCard-genres">${genre_ids ? getFilmGenres(genre_ids) : '-'}</p>
+            <p class="filmCard-release">${release_date ? release_date.slice(0, 4) : '-' }</p>
             ${rating}
         </div>
         <div class="test__btn">
@@ -48,25 +48,20 @@ const createFilmCard = (
 function getFilmGenres(genre_ids) {
   const genresList = fetchDataFromStorage('genres');
   const genres = [];
-  if (genresList.length <= 3) {
+  if (genresList.length) {
     for (let id of genre_ids) {
       genresList.map(genre => {
         if (genre.id === id) {
-          return genres.push(genre.name);
+          genres.push(genre.name);
         }
       });
     }
-  } else {
-    for (let i = 0; i < 2; i += 1) {
-      genresList.map(genre => {
-        if (genre_ids[i] && genre.id === genre_ids[i]) {
-          return genres.push(genre.name);
-        }
-      });
-    }
-    genres.push('Other');
-  }
-  return genres.join(', ');
+  };
+  console.log(genres);
+  if (genres.length <= 3) { return genres.join(', '); }
+  else if (genres.length > 3) {
+    return (`${genres[0]}, ${genres[1]}, Other`);
+  };
 }
 
-export default createFilmCard;
+export {createFilmCard, getFilmGenres};
