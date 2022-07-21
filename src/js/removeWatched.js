a967f0889c21feb3e846a6977eccfc5c59848ad6
Rostyslav Marchenko <marchenko.r.7@gmail.com>
2022-07-21 23:09:05 +0300

    remove task

--- src/js/removeWatched.js
+++ src/js/removeWatched.js
@@ -1,3 +1,18 @@
-export default function removeWatched(e) {
+import { saveDataToStorage, fetchDataFromStorage } from './dataStorage';
 
+export default function removeWatched(e) {
+  if (e.target.className != 'rotating-button__checkbox') {
+    return;
+  }
+  if (localStorage.getItem('watchedResult') !== null) {
+    let watchedResult = fetchDataFromStorage('watchedResult');
+    const currentFilm = watchedResult
+      .map(obj => {
+        return obj.id;
+      })
+      .indexOf(Number(e.target.dataset.value));
+
+    watchedResult.splice(currentFilm, 1);
+    saveDataToStorage('watchedResult', watchedResult);
+  }
 }
