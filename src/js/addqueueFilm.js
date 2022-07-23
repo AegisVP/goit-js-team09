4415b21449909e56af302f76513bfc9af3fa8b6d
DaryaGrygorova <grygorova.darya@gmail.com>
2022-07-24 02:08:38 +0300

    refactoring func of add film to lib
    При создании модального окна и кнопок добавления фильма  в нём, обеспечено передачу в функцию-обработчик  объекта filmData. Это гарантирует, что в хранилище попадет именно тот объект по которому отрисовано модалку, и обеспечит доступ к этим данным до момента закрытия окна.
    Проверки внетри функций добавления  переписаны в соответствии с особенностями входящих данных

--- src/js/addqueueFilm.js
+++ src/js/addqueueFilm.js
@@ -1,31 +1,28 @@
-export default addQueued 
+export default addQueued;
 import { saveDataToStorage, fetchDataFromStorage } from './dataStorage';
 
-function addQueued(e) {
-   if (!e.target.classList.contains('js-queue')) {
-     return;
-   }
-   if (fetchDataFromStorage('queueResult') !== null) {
-      let queueResult = fetchDataFromStorage('queueResult')
-         if (queueResult.map(obj => {return obj.id}).indexOf(Number(e.target.dataset.value)) > -1) {
-            return
-         } else {
-            let allData = fetchDataFromStorage('requestResults')
-            for (let i = 0; i < allData.length; i++) {
-               if (allData[i].id == e.target.dataset.value) {
-                  e.target.innerHTML = "QUEUED";
-                  queueResult.push(allData[i])
-                  saveDataToStorage('queueResult', queueResult)
-               }
-            }
-         }
-   } else {
-      let allData = fetchDataFromStorage('requestResults')
-               for (let i = 0; i < allData.length; i++) {
-            if (allData[i].id == e.target.dataset.value) {
-               e.target.innerHTML = "QUEUED";
-               saveDataToStorage('queueResult', [allData[i]])
-            }
-         }
-      }
+function addQueued(e, filmData) {
+  if (!e.target.classList.contains('js-queue')) {
+    return;
+  }
+  let queueResult = fetchDataFromStorage('queueResult');
+  if (queueResult) {
+    if (
+      queueResult.length >= 1 &&
+      queueResult.find(obj => obj.id == e.target.dataset.value)
+    ) {
+      return;
+    } else if (
+      queueResult.length === 1 &&
+      queueResult.id != e.target.dataset.value
+    ) {
+      queueResult.push(filmData);
+      saveDataToStorage('queueResult', queueResult);
+    } else {
+      queueResult.push(filmData);
+      saveDataToStorage('queueResult', queueResult);
+    }
+  } else {
+    saveDataToStorage('queueResult', [filmData]);
+  }
 }
