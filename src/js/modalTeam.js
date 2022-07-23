ffee61e5aff35eb45447e46c5c6c8df749ec3dd0
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-23 13:50:51 +0300

    play btn

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -11,17 +11,55 @@ function openModalDev(e, modalDev) {
     .addEventListener('click', (event) => {
       console.log(event);
       switch (event.target.dataset.name) { 
+
         case ('Vitaliy'):
           logoEl.classList.add('visually-hidden');
           teamText.innerHTML = `<p>Привіт, я Віталік!</p>`;
           teamText.classList.remove('visually-hidden');
           break;
+        
         case ('Rostyslav'):
-
          logoEl.classList.add('visually-hidden');
           teamText.innerHTML = `<p>Привіт, я Ростислав!</p>`;
           teamText.classList.remove('visually-hidden');
-         break;
+          break;
+        
+        case ('Vladislav'):
+         logoEl.classList.add('visually-hidden');
+          teamText.innerHTML = `<p>Привіт, я Владислав!</p>`;
+          teamText.classList.remove('visually-hidden');
+          break;
+        
+        case ('Marina'):
+         logoEl.classList.add('visually-hidden');
+          teamText.innerHTML = `<p>Привіт, я Марька!</p>`;
+          teamText.classList.remove('visually-hidden');
+          break;
+        
+        case ('Sergei'):
+         logoEl.classList.add('visually-hidden');
+          teamText.innerHTML = `<p>Привіт, я Сергій!</p>`;
+          teamText.classList.remove('visually-hidden');
+          break;
+        
+        case ('Roman'):
+         logoEl.classList.add('visually-hidden');
+          teamText.innerHTML = `<p>Привіт, я Роман!</p>`;
+          teamText.classList.remove('visually-hidden');
+          break;
+        
+        case ('Stefan'):
+         logoEl.classList.add('visually-hidden');
+          teamText.innerHTML = `<p>Привіт, я Стефан!</p>`;
+          teamText.classList.remove('visually-hidden');
+          break;
+        
+        case ('Dariya'):
+         logoEl.classList.add('visually-hidden');
+          teamText.innerHTML = `<p>Привіт, я Дар'я!</p>`;
+          teamText.classList.remove('visually-hidden');
+          break;
+        
         default:
         
          logoEl.classList.remove('visually-hidden');
