2326eea2243a15afc781c01607eedff56870845e
DaryaGrygorova <grygorova.darya@gmail.com>
2022-07-23 17:10:41 +0300

    Додоли відображення вітання при анімації

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -8,89 +8,114 @@ function openModalDev(e, modalDev) {
 
   document
     .querySelector('.developer__grid')
-    .addEventListener('click', (event) => {
+    .addEventListener('click', event => {
       // console.log(event);
-      switch (event.target.dataset.name) { 
 
-        case ('Vitaliy'):
-          logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Віталік!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        
-        case ('Rostyslav'):
-         logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Ростислав!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        
-        case ('Vladislav'):
-         logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Владислав!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        
-        case ('Marina'):
-         logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Марька!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        
-        case ('Sergei'):
-         logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Сергій!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        
-        case ('Roman'):
-         logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Роман!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        
-        case ('Stefan'):
-         logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Стефан!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        
-        case ('Dariya'):
-         logoEl.classList.add('visually-hidden');
-          teamText.innerHTML = `<p>Привіт, я Дар'я!</p>`;
-          teamText.classList.remove('visually-hidden');
-          break;
-        case ('run'):
-          runAnimation();
-          break;
-        default:
-        
-         logoEl.classList.remove('visually-hidden');
-          teamText.classList.add('visually-hidden');
-          break;
+      if (event.target.dataset.name && event.target.dataset.name !== 'run') {
+        logoEl.classList.add('visually-hidden');
+        teamText.innerHTML = `<p class="dev-name">Hi, I am ${event.target.dataset.name}!</p><p>Nice to meet you!</>`;
+        teamText.classList.remove('visually-hidden');
+      } else if (!event.target.dataset.name) {
+        logoEl.classList.remove('visually-hidden');
+        teamText.classList.add('visually-hidden');
       }
-       
-     })
+
+      if (event.target.dataset.name === 'run') { runAnimation(logoEl, teamText); }
+
+      // switch (event.target.dataset.name) {
+
+      //   case ('Vitaliy'):
+      //     logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Віталік!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+
+      //   case ('Rostyslav'):
+      //    logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Ростислав!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+
+      //   case ('Vladislav'):
+      //    logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Владислав!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+
+      //   case ('Marina'):
+      //    logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Марька!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+
+      //   case ('Sergei'):
+      //    logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Сергій!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+
+      //   case ('Roman'):
+      //    logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Роман!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+
+      //   case ('Stefan'):
+      //    logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Стефан!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+
+      //   case ('Dariya'):
+      //    logoEl.classList.add('visually-hidden');
+      //     teamText.innerHTML = `<p>Привіт, я Дар'я!</p>`;
+      //     teamText.classList.remove('visually-hidden');
+      //     break;
+      //   case ('run'):
+      //     runAnimation(logoEl, teamText);
+      //     break;
+      //   default:
+
+      //    logoEl.classList.remove('visually-hidden');
+      //     teamText.classList.add('visually-hidden');
+      //     break;
+      // }
+    });
 }
 
-function runAnimation() { 
-  // ссылки
+function runAnimation(logoEl, teamText) {
   const gridRef = document.querySelector('.grid');
   const refs = gridRef.querySelectorAll('[data-play]');
-  
+
   const playRefs = [];
-  for (let i = 0; i < refs.length; i++){
+  for (let i = 0; i < refs.length; i++) {
     if (refs[i].dataset.play === i);
     playRefs.push(refs[i]);
   }
 
-  playRefs.sort((fistEl, secondEl) => fistEl.dataset.play - secondEl.dataset.play );
+  playRefs.sort(
+    (fistEl, secondEl) => fistEl.dataset.play - secondEl.dataset.play
+  );
 
-  for (let i = 0; i < playRefs.length; i++) {
+  for (let i = 0; i <= playRefs.length; i++) {
     const delay = i * 1000;
-    setTimeout(() => { playRefs[i].classList.add('animation')}, delay);
-    setTimeout(() => playRefs[i].classList.remove('animation'), delay + 1000);
+    if (i < playRefs.length) {
+      setTimeout(() => {
+        playRefs[i].classList.add('animation');
+        logoEl.classList.add('visually-hidden');
+        teamText.innerHTML = `<p>Hi, I am ${playRefs[i].dataset.devname}!</p>`;
+        teamText.classList.remove('visually-hidden');
+      }, delay);
+      setTimeout(() => playRefs[i].classList.remove('animation'), delay + 1000);
+    } else {
+      {
+        setTimeout(() => {
+          logoEl.classList.remove('visually-hidden');
+          teamText.classList.add('visually-hidden');
+        }, delay);
+      }
+    }
   }
 }
 
-
-export {openModalDev}
+export { openModalDev };
