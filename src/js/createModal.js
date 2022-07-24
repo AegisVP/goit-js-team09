5746dabadbeaffe00b2e01e80f18995f2f6e6957
AegisVP <vlad@pysarenko.com>
2022-07-24 23:38:10 +0200

    add navigation notice to modal

--- src/js/createModal.js
+++ src/js/createModal.js
@@ -19,15 +19,15 @@ function createModal(filmData) {
 
 	return `
   <div class="modal-main" data-id="${filmData.id}" data-base="${base}" data-index="${index}">
-      <button type="button" class="modal__close-button modal-main__btn-close" data-action="close-modal">
-        <svg class="modal-main__close-icon" width="14" height="14" viewBox="0 0 32 32">
-          <symbol id="close">
-            <path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path>
-          </symbol>
-          <use href="#close"></use>
-        </svg>
-      </button>
-      <div class="modal-main__film-wrap">
+    <button type="button" class="modal__close-button modal-main__btn-close" data-action="close-modal">
+      <svg class="modal-main__close-icon" width="14" height="14" viewBox="0 0 32 32">
+        <symbol id="close">
+          <path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path>
+        </symbol>
+        <use href="#close"></use>
+      </svg>
+    </button>
+    <div class="modal-main__film-wrap">
       <div class="modal-main__film-poster list">
         <img class="film__img" srcset="${imgSrc(400)} 1x, ${imgSrc(500)} 2x" src="${imgSrc(400)}" width="280" height="398" alt="${filmData.title ? filmData.title : 'Poster'}"/>
         </div>
@@ -71,16 +71,15 @@ function createModal(filmData) {
           </div>
           <div class="rotating-button__wrapper">
             <label>
-              <input type="checkbox" name="watched" class="rotating-button__checkbox js-watched" data-label="watched" data-value="${filmData.id}" ${
-		watchedChecked ? 'checked' : ''
-	}/>
+              <input type="checkbox" name="watched" class="rotating-button__checkbox js-watched" data-label="watched" data-value="${filmData.id}" ${watchedChecked ? 'checked' : ''}/>
               <span type="button" class="rotating-button__button-on">Add to watched</span>
               <span type="button" class="rotating-button__button-off">Remove from watched</span>
             </label>
           </div>
-          </div>
         </div>
       </div>
+    </div>
+  <p class="modal-main__notice">To navigate, press left and right arrow keys</p>
   </div>`;
 }
 
