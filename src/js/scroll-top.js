74f86cb62fd26e76d1c6ad93619cd2942a8c69a0
AegisVP <vlad@pysarenko.com>
2022-07-24 14:06:14 +0200

    Scroll to top added

---
+++ src/js/scroll-top.js
@@ -0,0 +1,10 @@
+import debounce from './lodash-debounce';
+const topBtn = document.querySelector('[data-scroll-top]');
+
+window.addEventListener('scroll', debounce(scrollFunction, 250, { leading: true, trailing: true }));
+
+function scrollFunction() {
+	console.log(document.documentElement.scrollTop);
+	if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) topBtn.classList.add('visible');
+	else topBtn.classList.remove('visible');
+}
