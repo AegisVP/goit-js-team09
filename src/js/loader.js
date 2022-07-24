f88e93caa1d9c8533efb654510e62f22a75df168
AegisVP <vlad@pysarenko.com>
2022-07-24 05:29:25 +0200

    fix loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,8 +1,8 @@
 const showLoader = status => {
 	const loaderEl = document.querySelector('.loader__backdrop');
-	if (status)
-		if (loaderEl.classList.contains('is-hidden')) loaderEl.classList.remove('is-hidden');
-		else loaderEl.classList.add('is-hidden');
+
+	if (status) loaderEl.classList.remove('is-hidden');
+	else loaderEl.classList.add('is-hidden');
 };
 
 export default showLoader;
