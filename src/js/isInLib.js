c9aade77795877cbfc931faaceed0cb0986e17b1
AegisVP <vlad@pysarenko.com>
2022-07-24 02:41:11 +0200

    fix

--- src/js/isInLib.js
+++ src/js/isInLib.js
@@ -4,9 +4,10 @@ function isInLib({id, storageKey}) {
   const data = fetchDataFromStorage(storageKey);
   const ID = Number(id);
   if (data) {
-  for (let i = 0; i < data?.length; i++) {
-    if (data[i].id === ID) {
-      return data[i];
+    for (let i = 0; i < data?.length; i++) {
+      if (data[i].id === ID) {
+        return data[i];
+      }
     }
   }
   return;
