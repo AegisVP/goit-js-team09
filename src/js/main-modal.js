4139d1a40e1a6fa69613ee7659752fa63148b294
AegisVP <vlad@pysarenko.com>
2022-07-25 02:04:53 +0200

    add swipe support

--- src/js/main-modal.js
+++ src/js/main-modal.js
@@ -5,6 +5,8 @@ const KEY_CODE_ESC = 'Escape';
 const KEY_CODE_ARROW_LEFT = 'ArrowLeft';
 const KEY_CODE_ARROW_RIGHT = 'ArrowRight';
 let modalWindowRef = null;
+let xDown = null;
+let yDown = null;
 
 function onOpenModal(id, elementRef, storageBases = ['requestResults', 'watchedResult', 'queueResult']) {
 	const ID = Number(id);
@@ -19,22 +21,58 @@ function onOpenModal(id, elementRef, storageBases = ['requestResults', 'watchedR
 
 	modalWindowRef.innerHTML = createModal(filmData);
 	document.body.classList.add('show-modal');
+	
+	document.querySelector('[data-action="close-modal"]').addEventListener('click', onCloseModal);
+	document.querySelector('.rotating-button__wrapper input[name="queue"]').addEventListener('change', e => selectAddDelete(e, filmData));
+	document.querySelector('.rotating-button__wrapper input[name="watched"]').addEventListener('change', e => selectAddDelete(e, filmData));
 
 	// add listeners
 	window.addEventListener('keydown', onKeyPress);
-	document.querySelector('[data-action="close-modal"]').addEventListener('click', onCloseModal);
-	document.querySelector('.js-backdrop').addEventListener('click', onBackdropClick);
-	document.querySelector('.rotating-button__wrapper input[name="queue"]').addEventListener('change', e => selectAddDelete(e, filmData));
-	document.querySelector('.rotating-button__wrapper input[name="watched"]').addEventListener('change', e => selectAddDelete(e, filmData));
+	document.addEventListener('touchstart', handleTouchStart);
+	document.addEventListener('touchmove', handleTouchMove);
+	modalWindowRef.addEventListener('click', onBackdropClick);
 }
 
 function onCloseModal() {
-	window.removeEventListener('keydown', onKeyPress);
 	document.body.classList.remove('show-modal');
+
+	// remove listeners
+	window.removeEventListener('keydown', onKeyPress);
+	document.removeEventListener('touchstart', handleTouchStart);
+	document.removeEventListener('touchmove', handleTouchMove);
+	modalWindowRef.removeEventListener('click', onBackdropClick);
 }
 
 function onBackdropClick(event) {
 	if (event.currentTarget === event.target) onCloseModal();
+}
+
+function getTouches(e) {
+	return e.touches || e.originalEvent.touches;
+}
+
+function handleTouchStart(e) {
+	const firstTouch = getTouches(e)[0];
+	xDown = firstTouch.clientX;
+	yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(e) {
+	if (!xDown || !yDown) return;
+
+	let xUp = e.touches[0].clientX;
+	let yUp = e.touches[0].clientY;
+
+	let xDiff = xDown - xUp;
+	let yDiff = yDown - yUp;
+	
+	if (Math.abs(xDiff) > Math.abs(yDiff)) {
+		if (xDiff > 0) nextFilm();
+		else prevFilm();
+	}
+
+	xDown = null;
+	yDown = null;
 }
 
 function onKeyPress(event) {
